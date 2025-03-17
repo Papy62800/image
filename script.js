6c649ee6bfa9d156c34fabd0e7a44a6611d23781
antoine <antoine.demessine@gmail.com>
2025-03-17 13:36:11 +0100

    2éméMAJ

---
+++ script.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById("tt");
+
+btn.addEventListener("click", openMenu);
+
+function openMenu() {
+    window.open('https://carnot-snt.enseigne.ac-lyon.fr/snt_web/img/site-en-construction-04_clrFFFFFF.png', '_blank');
+  console.log(btn);
+}
